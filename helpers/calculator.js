6326fc8946f4e40666545075933b284937529458
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-08 16:51:14 +0200

    refactor: should calculate percentage from account

--- helpers/calculator.js
+++ helpers/calculator.js
@@ -7,24 +7,17 @@
  * @param {Array} amounts - Array of amounts
  * @returns {Array} - Array of percentages
  */
-function calculatePercentages(amounts) {
-  // Calculate the sum of all amounts
-  const totalAmount = amounts.reduce((sum, amount) => sum + amount, 0)
+const calculatePercentages = amounts => {
+  const sum = amounts.reduce((sum, amount) => sum + amount, 0)
   
-  // Calculate the percentages as integers and keep track of the remaining
-  let remaining = 100
-  const percentages = amounts.map(amount => {
-    const percentage = Math.floor((amount / totalAmount) * 100)
-    remaining -= percentage
-    return percentage
-  })
+  const percentages = amounts.map(
+    amount => _convertFloatToPercentage(amount / sum)
+  )
 
-  // Add the remaining percentage to the last value
-  percentages[percentages.length - 1] += remaining
-
-  // Return the percentages as an array of integers
   return percentages
 }
+
+const _convertFloatToPercentage = float => Math.floor(float * 100)
 
 module.exports = {
   calculatePercentages
