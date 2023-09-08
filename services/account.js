3c8a38d7d574fd4b50a7082b175b14503e5f4ed7
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-08 19:37:47 +0200

    test(green): account service calls the calculator

--- services/account.js
+++ services/account.js
@@ -1,5 +1,15 @@
-const setDesiredAllocationPercentage = () => {
-  
+const calculator = require('../helpers/calculator')
+
+/**
+ * @param {object} accounts - accounts object where key is the 
+ * type and value is the amount
+ * @returns {void}
+ * @description sets the desired allocation percentage for each
+ * account type
+ */
+const setDesiredAllocationPercentage = accounts => {
+  const amounts = Object.values(accounts)
+  calculator.calculatePercentages(amounts)
 }
 
 module.exports = {
