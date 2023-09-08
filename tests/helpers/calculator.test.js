30d18cd7589099d6c9fa21eda34c31a8d8650184
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-08 16:58:12 +0200

    test(red): calculator - integers should add up to 100

--- tests/helpers/calculator.test.js
+++ tests/helpers/calculator.test.js
@@ -2,8 +2,15 @@ const { expect } = require('chai')
 const calculator = require('../../helpers/calculator')
 
 describe('Calculator', () => {
-  it('should calculate percentage correctly', () => {
-    expect(calculator.calculatePercentages([6000, 3000, 1000]))
-      .to.eql([60, 30, 10])
+  describe('calculatePercentages', () => {
+    it('should calculate percentage correctly', () => {
+      expect(calculator.calculatePercentages([6000, 3000, 1000]))
+        .to.eql([60, 30, 10])
+    })
+  
+    it('should return integers that sum up to 100', () => {
+      expect(calculator.calculatePercentages([3000, 3000, 3000]))
+        .to.eql([33, 33, 34])
+    })
   })
 })
