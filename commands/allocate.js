4c558e6662bd517ed2ea79a5af466c1771f46018
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-08 15:07:31 +0200

    refactor: should parse allocate input in correct order

--- commands/allocate.js
+++ commands/allocate.js
@@ -1,13 +1,15 @@
 const { Command } = require('commander')
 const logger = require('../logger')
 
+const _handleAllocate = (equity, debt, gold) => {
+  logger.info(`ALLOCATE EQUITY:${equity}, DEBT:${debt}, GOLD:${gold}`)
+}
+
 const allocate = new Command('allocate')
   .description('receives the initial investment amounts for each fund.')
   .argument('<equity>', 'equity investment amount')
   .argument('<debt>', 'debt investment amount')
   .argument('<gold>', 'gold investment amount')
-  .action((equity, debt, gold) => {
-    logger.info(`ALLOCATE EQUITY:${equity}, DEBT:${debt}, GOLD:${gold}`)
-  })
+  .action(_handleAllocate)
 
 module.exports = allocate
