3cac077ee28f241dd878a68fccd8de12d6700a3c
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-08 19:35:14 +0200

    test(red): account service calls the calculator

---
+++ tests/services/account.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon')
+const { expect } = require('chai')
+const calculator = require('../../helpers/calculator')
+const { accountService } = require('../../services')
+
+
+describe('Account Service', () => {
+  describe('setDesiredAllocationPercentage()', () => {
+    it('should call the calculator helper to calculate percentages', () => {
+      const calculatorStub = sinon.stub(calculator, 'calculatePercentages')
+      const accounts = {
+        equity: 100,
+        debt: 100,
+        gold: 100
+      }
+      accountService.setDesiredAllocationPercentage(accounts)
+      expect(calculatorStub.calledOnce).to.be.true
+      calculatorStub.restore()
+    })
+  })
+})
