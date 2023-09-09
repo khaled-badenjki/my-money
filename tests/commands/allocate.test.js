7765958f0d5810ce9e526ab877b9b8e824b19ed1
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-09 15:00:22 +0200

    test(red): allocate should throw an error if nothing is passed

--- tests/commands/allocate.test.js
+++ tests/commands/allocate.test.js
@@ -5,96 +5,124 @@ const logger = require('../../src/helpers/logger')
 const { accountService, operationService } = require('../../src/services')
 
 describe('commands/allocate', () => {
-  let loggerStub
-  let accountServiceStub
-  let operationServiceStub
+  describe('input validation', () => {
+    let loggerStub
+    let processExitStub
+  
+    before(() => {
+      loggerStub = sinon.stub(logger, 'error')
+      processExitStub = sinon.stub(process, 'exit')
 
-  before(() => {
-    loggerStub = sinon.stub(logger, 'info')
-    accountServiceStub = sinon
-      .stub(accountService, 'setDesiredAllocationPercentage')
-      .resolves()
-    operationServiceStub = sinon
-      .stub(operationService, 'createAllocations')
-      .resolves()
+    })
+  
+    after(() => {
+      loggerStub.restore()
+      processExitStub.restore()
+    })
+  
+    it('should throw an error if no arguments are passed', () => {
+      program.parse(['node', 'index.js', 'allocate'])
+  
+      processExitStub.callsFake((code) => {
+        expect(code).to.equal(1) // Check that the exit code is 1
+      })
+      expect(loggerStub.calledWith(sinon.match(/Invalid input/))).to.be.true
+
+    })
   })
-
-  after(() => {
-    loggerStub.restore()
-    accountServiceStub.restore()
-    operationServiceStub.restore()
-  })
-
-  it('should log ALLOCATE as info', () => {
-    program.parse(['node', 'index.js', 'allocate', '6000', '3000', '1000'])
-
-    expect(loggerStub.calledWith(sinon.match(/ALLOCATE/))).to.be.true
-  })
-
-  it('should parse the input values in correct order', () => {
-    const params = {
-      equity: '6000',
-      debt: '3000',
-      gold: '1000'
-    }
-
-    program.parse(['node', 'index.js', 'allocate',
-      params.equity, 
-      params.debt, 
-      params.gold
-    ])
-
-    expect(program.args).to.deep.equal([
-      'allocate',
-      params.equity, 
-      params.debt, 
-      params.gold
-    ])
-
-    expect(loggerStub.calledWith(
-      `ALLOCATE EQUITY:${params.equity}, ` +
-      `DEBT:${params.debt}, ` +
-      `GOLD:${params.gold}`
-    )).to.be.true
-  })
-
-  it ('should call the account service with the correct params', () => {
-    const params = {
-      equity: '6000',
-      debt: '3000',
-      gold: '1000'
-    }
-
-    program.parse(['node', 'index.js', 'allocate',
-      params.equity, 
-      params.debt, 
-      params.gold
-    ])
-
-    expect(accountServiceStub.calledWith({
-      equity: params.equity, 
-      debt: params.debt, 
-      gold: params.gold
-    })).to.be.true
-  })
-
-  it('should call the operation service with the correct params', () => {
-    const params = {
-      equity: '6000',
-      debt: '3000',
-      gold: '1000'
-    }
-
-    program.parse(['node', 'index.js', 'allocate',
-      params.equity, 
-      params.debt, 
-      params.gold
-    ])
-
-    expect(operationServiceStub.calledWith({
-      equity: params.equity, 
-      debt: params.debt, 
-      gold: params.gold
-    })).to.be.true
+  
+  describe('interaction', () => {
+    let loggerStub
+    let accountServiceStub
+    let operationServiceStub
+  
+    before(() => {
+      loggerStub = sinon.stub(logger, 'info')
+      accountServiceStub = sinon
+        .stub(accountService, 'setDesiredAllocationPercentage')
+        .resolves()
+      operationServiceStub = sinon
+        .stub(operationService, 'createAllocations')
+        .resolves()
+    })
+  
+    after(() => {
+      loggerStub.restore()
+      accountServiceStub.restore()
+      operationServiceStub.restore()
+    })
+  
+    it('should log ALLOCATE as info', () => {
+      program.parse(['node', 'index.js', 'allocate', '6000', '3000', '1000'])
+  
+      expect(loggerStub.calledWith(sinon.match(/ALLOCATE/))).to.be.true
+    })
+  
+    it('should parse the input values in correct order', () => {
+      const params = {
+        equity: '6000',
+        debt: '3000',
+        gold: '1000'
+      }
+  
+      program.parse(['node', 'index.js', 'allocate',
+        params.equity, 
+        params.debt, 
+        params.gold
+      ])
+  
+      expect(program.args).to.deep.equal([
+        'allocate',
+        params.equity, 
+        params.debt, 
+        params.gold
+      ])
+  
+      expect(loggerStub.calledWith(
+        `ALLOCATE EQUITY:${params.equity}, ` +
+        `DEBT:${params.debt}, ` +
+        `GOLD:${params.gold}`
+      )).to.be.true
+    })
+  
+    it ('should call the account service with the correct params', () => {
+      const params = {
+        equity: '6000',
+        debt: '3000',
+        gold: '1000'
+      }
+  
+      program.parse(['node', 'index.js', 'allocate',
+        params.equity, 
+        params.debt, 
+        params.gold
+      ])
+  
+      expect(accountServiceStub.calledWith({
+        equity: params.equity, 
+        debt: params.debt, 
+        gold: params.gold
+      })).to.be.true
+    })
+  
+    it('should call the operation service with the correct params', () => {
+      const params = {
+        equity: '6000',
+        debt: '3000',
+        gold: '1000'
+      }
+  
+      program.parse(['node', 'index.js', 'allocate',
+        params.equity, 
+        params.debt, 
+        params.gold
+      ])
+  
+      expect(operationServiceStub.calledWith({
+        equity: params.equity, 
+        debt: params.debt, 
+        gold: params.gold
+      })).to.be.true
+    })
   })
 })
