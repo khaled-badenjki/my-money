5b90c52de68f763249a14517450b5e074b346d2c
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-09 17:50:30 +0200

    refactor: account service calls account model with correct args

--- tests/services/account.test.js
+++ tests/services/account.test.js
@@ -7,27 +7,14 @@ const db = require('../../src/dal/models')
 
 describe('Account Service', () => {
   describe('setDesiredAllocationPercentage()', () => {
-    let calculatorStub
     let accountBulkCreateStub
 
     before(() => {
-      calculatorStub = sinon.stub(calculator, 'calculatePercentages')
       accountBulkCreateStub = sinon.stub(db.Account, 'bulkCreate')
     })
 
     after(() => {
-      calculatorStub.restore()
       accountBulkCreateStub.restore()
-    })
-
-    it('should call the calculator helper to calculate percentages', () => {
-      const accounts = {
-        equity: 100,
-        debt: 100,
-        gold: 100
-      }
-      accountService.setDesiredAllocationPercentage(accounts)
-      expect(calculatorStub.calledOnce).to.be.true
     })
 
     it('should call account model bulk create', async () => {
