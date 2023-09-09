5b2c8b67e1abe316e938b3cc23d3d32622418328
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-09 09:32:03 +0200

    test(red): account service calls account model

--- tests/services/account.test.js
+++ tests/services/account.test.js
@@ -2,18 +2,22 @@ const sinon = require('sinon')
 const { expect } = require('chai')
 const calculator = require('../../src/helpers/calculator')
 const { accountService } = require('../../src/services')
+const db = require('../../src/dal/models')
 
 
 describe('Account Service', () => {
   describe('setDesiredAllocationPercentage()', () => {
     let calculatorStub
+    let accountBulkCreateStub
 
     before(() => {
       calculatorStub = sinon.stub(calculator, 'calculatePercentages')
+      accountBulkCreateStub = sinon.stub(db.account, 'bulkCreate')
     })
 
     after(() => {
       calculatorStub.restore()
+      accountBulkCreateStub.restore()
     })
 
     it('should call the calculator helper to calculate percentages', () => {
@@ -24,6 +28,16 @@ describe('Account Service', () => {
       }
       accountService.setDesiredAllocationPercentage(accounts)
       expect(calculatorStub.calledOnce).to.be.true
+    })
+
+    it('should call account model bulk create', () => {
+      const accounts = {
+        equity: 100,
+        debt: 100,
+        gold: 100
+      }
+      accountService.setDesiredAllocationPercentage(accounts)
+      expect(accountBulkCreateStub.calledOnce).to.be.true
     })
   })
 })
