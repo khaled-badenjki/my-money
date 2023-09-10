7b50e2aae7a594f27e58d4918ca9ae1b989dbc7a
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-10 05:36:42 +0200

    test(red): operation service calls account model with correct args

--- tests/services/operation.test.js
+++ tests/services/operation.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon')
 const { expect } = require('chai')
 const calculator = require('../../src/helpers/calculator')
-const { operationService } = require('../../src/services')
+const { operationService, accountService } = require('../../src/services')
 const db = require('../../src/dal/models')
 
 describe('Operation Service', () => {
@@ -24,6 +24,39 @@ describe('Operation Service', () => {
       }
       operationService.createAllocations(accounts)
       expect(operationBulkCreateStub.called).to.be.true
+    })
+
+    it('should call operation model bulk create with correct arguments', () => {
+      const accounts = {
+        equity: 6000,
+        debt: 2000,
+        gold: 2000
+      }
+
+      operationService.createAllocations(accounts)
+
+      const expectedOperations = [
+        {
+          type: 'allocation',
+          amount: 6000,
+          accountId: 1,
+          date: new Date()
+        },
+        {
+          type: 'allocation',
+          amount: 2000,
+          accountId: 2,
+          date: new Date()
+        },
+        {
+          type: 'allocation',
+          amount: 2000,
+          accountId: 3,
+          date: new Date()
+        }
+      ]
+
+      expect(operationBulkCreateStub.calledWith(expectedOperations)).to.be.true
     })
   })
 })
