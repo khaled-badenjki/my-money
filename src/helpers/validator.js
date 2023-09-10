fdbbe61bf567275dbeeff22800597727f955b689
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-10 22:50:53 +0200

    refactor: change should fail if first 3 args are not percentages

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,31 +1,21 @@
-const validateAllocateInput = arr => {
-  return _validateExists(arr) && 
-    _validateIsNumber(arr) && 
-    _validateIsPositive(arr)
-}
+const validateAllocateInput = arr => _validateExists(arr) 
+  && _validateIsNumber(arr) 
+  && _validateIsPositive(arr)
 
 const validateSipInput = validateAllocateInput
 
-const validateChangeInput = arr => {
-  return _validateExists(arr) && 
-    _validateIsPercentage(arr)
-}
+const validateChangeInput = arr => _validateExists(arr) 
+  && _validateIsPercentage(arr)
 
-const _validateExists = arr => {
-  return arr.some(amount => !amount) ? false : true
-}
+const _validateExists = arr => arr.some(amount => !amount) ? false : true
 
-const _validateIsNumber = arr => {
-  return arr.some(amount => isNaN(amount)) ? false : true
-}
+const _validateIsNumber = arr => arr.some(amount => isNaN(amount)) 
+  ? false : true
 
-const _validateIsPositive = arr => {
-  return arr.some(amount => amount < 0) ? false : true
-}
+const _validateIsPositive = arr => arr.some(amount => amount < 0) ? false : true
 
-const _validateIsPercentage = arr => {
-  return arr.some(amount => amount.endsWith('%')) ? true : false
-}
+const _validateIsPercentage = arr => arr.some(amount => amount.endsWith('%')) 
+  ? true : false
 
 module.exports = {
   validateAllocateInput,
