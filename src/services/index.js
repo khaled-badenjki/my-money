139ddf5de8d4e6bc106f7a55ea531eb7c456f27f
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-11 16:59:33 +0200

    test(green): allocate service calls account model

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,22 @@
 const account = require('./account')
 const operation = require('./operation')
+const calculator = require('../helpers/calculator')
+const db = require('../dal/models')
 
-const allocate = async () => {
+const allocate = async accounts => {
+  const amounts = accounts.map(account => account.amount)
+  const percentages = calculator.calculatePercentages(amounts)
 
+  const dbAccounts = await db.Account.bulkCreate(
+    accounts.map((account, index) => ({
+      name: account.name,
+      desiredAllocationPercentage: percentages[index]
+    }))
+  )
+
+  await db.sequelize.close()
+
+  return dbAccounts
 }
 
 module.exports = {
