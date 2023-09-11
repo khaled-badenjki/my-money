b16764774c2493bb32d7209f874799b0d6e22a1b
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-11 23:37:00 +0200

    test(green): balance log should be sorted

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -2,6 +2,8 @@ const { Command } = require('commander')
 const { logger } = require('../helpers/logger')
 const services = require('../services')
 
+const ACCOUNTS_ORDER = ['equity', 'debt', 'gold']
+
 const balance = new Command('BALANCE')
   .description('receives a month name')
   .argument('<month>', 'month')
@@ -11,6 +13,10 @@ const balance = new Command('BALANCE')
 const _handleBalance = async month => {
   try {
     const balance = await services.balance(month)
+    // order the output as equity -> debt -> gold
+    balance.sort((a, b) => 
+      ACCOUNTS_ORDER.indexOf(a.name) - ACCOUNTS_ORDER.indexOf(b.name))
+      
     logger.info(balance.map(b => b.balance).join(' '))
   } catch (error) {
     logger.error(error.message)
