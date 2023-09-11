acf786eeb6a2417d4f61dccdec75a916a3dd493d
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-11 23:07:12 +0200

    test(red): balance should log out accounts balance

--- tests/commands/balance.test.js
+++ tests/commands/balance.test.js
@@ -31,5 +31,23 @@ describe('commands/balance', () => {
       services.balance.restore()
       logger.error.restore()
     })
+
+    it('should log the balance', async () => {
+      sinon.stub(services, 'balance').returns([
+        { name: 'equity', balance: 1000 },
+        { name: 'debt', balance: 5000 },
+        { name: 'gold', balance: 2000 },
+
+      ])
+      sinon.stub(logger, 'info')
+
+      await callBalance(['APRIL'])
+
+      expect(logger.info.calledOnce).to.be.true
+      expect(logger.info.calledWith('1000 5000 2000')).to.be.true
+
+      services.balance.restore()
+      logger.info.restore()
+    })
   })
 })
