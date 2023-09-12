1b237eb7227b5b89d5c51bb6e5a5ff6b80740923
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-12 22:13:53 +0200

    test(red): validation throws an error

--- tests/helpers/validator.test.js
+++ tests/helpers/validator.test.js
@@ -3,68 +3,92 @@ const validator = require('../../src/helpers/validator')
 
 describe('Validator', () => {
   describe('validateAllocateInput', () => {
-    it('should return true if all inputs are valid', () => {
+    it('should pass if all inputs are valid', () => {
       const input = ['100', '200', '300']
       expect(validator.validateAllocateInput(input)).to.be.true
+      expect(() => validator.validateAllocateInput(input)).not.to.throw()
     })
 
-    it('should return false if any input is missing', () => {
+    it('should throw an error if any input is missing', () => {
       const input = ['100', '200', '']
-      expect(validator.validateAllocateInput(input)).to.be.false
+      // expect it to throw an error
+      expect(() => validator.validateAllocateInput(input))
+        .to.throw('MISSING_INPUT')
     })
 
-    it('should return false if any input is not a number', () => {
+    it('should fail if any input is not a number', () => {
       const input = ['100', '200', 'abc']
-      expect(validator.validateAllocateInput(input)).to.be.false
+      expect(() => validator.validateAllocateInput(input))
+        .to.throw('INPUT_NOT_NUMBER')
     })
 
-    it('should return false if any input is negative', () => {
+    it('should fail if any input is negative', () => {
       const input = ['100', '200', '-300']
-      expect(validator.validateAllocateInput(input)).to.be.false
+      expect(() => validator.validateAllocateInput(input))
+        .to.throw('INPUT_NEGATIVE')
     })
   })
 
   describe('validateSipInput', () => {
-    it('should return true if all inputs are valid', () => {
+    it('should pass if all inputs are valid', () => {
       const input = ['100', '200', '300']
       expect(validator.validateSipInput(input)).to.be.true
+      expect(() => validator.validateSipInput(input)).not.to.throw()
     })
 
-    it('should return false if any input is missing', () => {
+    it('should fail if any input is missing', () => {
       const input = ['100', '200', '']
-      expect(validator.validateSipInput(input)).to.be.false
+      expect(() => validator.validateSipInput(input)).to.throw('MISSING_INPUT')
     })
 
-    it('should return false if any input is not a number', () => {
+    it('should fail if any input is not a number', () => {
       const input = ['100', '200', 'abc']
-      expect(validator.validateSipInput(input)).to.be.false
+      expect(() => validator.validateSipInput(input))
+        .to.throw('INPUT_NOT_NUMBER')
     })
 
-    it('should return false if any input is negative', () => {
+    it('should fail if any input is negative', () => {
       const input = ['100', '200', '-300']
-      expect(validator.validateSipInput(input)).to.be.false
+      expect(() => validator.validateSipInput(input))
+        .to.throw('INPUT_NEGATIVE')
     })
   })
 
   describe('validateChangeInput', () => {
-    it('should return true if all inputs are valid', () => {
-      const input = ['100%', '200%', '300%']
+    it('should pass if all inputs are valid', () => {
+      const input = ['100%', '200%', '300%', 'JANUARY']
       expect(validator.validateChangeInput(input)).to.be.true
+      expect(() => validator.validateChangeInput(input)).not.to.throw()
     })
 
-    it('should return false if any input is missing', () => {
-      const input = ['100%', '200%', '']
-      expect(validator.validateChangeInput(input)).to.be.false
+    it('should pass if input is in negative', () => {
+      const input = ['-100%', '-200%', '-300%', 'JANUARY']
+      expect(validator.validateChangeInput(input)).to.be.true
+      expect(() => validator.validateChangeInput(input)).not.to.throw()
     })
 
-    it('should return false if any input is not a number', () => {
-      const input = ['100%', '200%', 'abc%']
-      expect(validator.validateChangeInput(input)).to.be.false
+    it('should fail if month name is invalid', () => {
+      const input = ['100%', '200%', '300%', 'JANUARYY']
+      expect(() => validator.validateChangeInput(input))
+        .to.throw('INVALID_MONTH')
     })
 
-    it('should return false if any input is not a percentage', () => {
+    it('should fail if any input is missing', () => {
+      const input = ['100%', '200%', '', 'JANUARY']
+      expect(() => validator.validateChangeInput(input))
+        .to.throw('MISSING_INPUT')
+    })
+
+    it('should fail if any input is not a number', () => {
+      const input = ['100%', '200%', 'abc%', 'JANUARY']
+      expect(() => validator.validateChangeInput(input))
+        .to.throw('INPUT_NOT_NUMBER')
+    })
+
+    it('should fail if any input is not a percentage', () => {
       const input = ['100', '200', '300']
-      expect(validator.validateChangeInput(input)).to.be.false
+      expect(() => validator.validateChangeInput(input))
+        .to.throw('INPUT_NOT_PERCENTAGE')
     })
   })
 })
