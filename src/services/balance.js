d2478eb648ee29b6e2bc8cb603ba5e2eb0919257
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 23:28:12 +0200

    test(green): balance should be filtered by month

--- src/services/balance.js
+++ src/services/balance.js
@@ -1,7 +1,7 @@
 const db = require('../dal/models')
 
 const execute = async month => {
-  const sum = await db.Operation.findAll(_buildSumQuery())
+  const sum = await db.Operation.findAll(_buildSumQuery(month))
   const accounts = await db.Account.findAll({ raw: true })
 
   const balance = accounts.map(account => {
@@ -15,14 +15,19 @@ const execute = async month => {
   return balance
 }
 
-const _buildSumQuery = () => {
+const _buildSumQuery = (month) => {
   return {
     attributes: [
       'accountId',
       [db.sequelize.fn('sum', db.sequelize.col('amount')), 'total']
     ],
     group: ['accountId'],
-    raw: true
+    raw: true,
+    where: {
+      date: {
+        [db.Sequelize.Op.lte]: `2023-${month}-16`
+      }
+    }
   }
 }
 
