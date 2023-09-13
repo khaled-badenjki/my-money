0f77ab013380aaf2440d83cd73f8f18b79731b89
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 14:26:38 +0200

    refactor: change service logic

--- src/services/change.js
+++ src/services/change.js
@@ -8,10 +8,19 @@ const execute = async (accountsChangePercentage, month) => {
     raw: true
   })
 
-  const operations
-    = _buildOperations(accounts, sum, month, accountsChangePercentage).flat()
+  const operations = accounts.map((account, index) => {
+    let total = _getAccountTotal(sum, account)
 
-  await db.Operation.bulkCreate(operations)
+    if (_sipIsApplicable(month)) total += account.sip
+
+    const change = 
+      _changePercentToAmount(total, accountsChangePercentage[index].change)
+
+    return _buildOperations(account, change, month)
+  })
+
+
+  await db.Operation.bulkCreate(operations.flat())
 
   return sum
 }
@@ -27,25 +36,27 @@ const _buildSumQuery = () => {
   }
 }
 
-const _buildOperations = (accoutnsArr, sum, month, changeArr) => 
-  accoutnsArr.map((account, index) => {
-    let total = parseInt(sum.find(s => s.accountId === account.id).total)
-    if (month >= defaults.SIP_START_MONTH) {
-      total += account.sip
-    }
-    const change = total * (changeArr[index].change / 100)
-    return [{
-      type: 'sip',
-      amount: account.sip,
-      accountId: account.id,
-      date: `${defaults.YEAR}-${month}-${defaults.DAY}`
-    }, {
-      type: 'change',
-      amount: change,
-      accountId: account.id,
-      date: `${defaults.YEAR}-${month}-${defaults.DAY}`
-    }]
-})
+const _buildOperations = (account, change, month) => 
+  [{
+    type: 'sip',
+    amount: account.sip,
+    accountId: account.id,
+    date: `${defaults.YEAR}-${month}-${defaults.DAY}`
+  }, {
+    type: 'change',
+    amount: change,
+    accountId: account.id,
+    date: `${defaults.YEAR}-${month}-${defaults.DAY}`
+  }].flat()
+
+const _sipIsApplicable = month => 
+  month >= defaults.SIP_START_MONTH
+
+const _getAccountTotal = (sum, account) =>
+  parseInt(sum.find(s => s.accountId === account.id).total)
+
+const _changePercentToAmount = (total, change) =>
+  total * (change / 100)
 
 module.exports = {
   execute
