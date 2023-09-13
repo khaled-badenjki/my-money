83b90c0969db4124a394abe19a74c36d8e2c6312
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 07:24:35 +0200

    feat: add transaction to allocate service function

--- src/services/allocate.js
+++ src/services/allocate.js
@@ -8,21 +8,23 @@ const execute = async accounts => {
   const amounts = accounts.map(account => account.amount)
   const percentages = calculator.calculatePercentages(amounts)
 
-  const dbAccounts = await db.Account.bulkCreate(
-    accounts.map((account, index) => ({
-      name: account.name,
-      desiredAllocationPercentage: percentages[index]
-    }))
-  )
+  await db.sequelize.transaction(async t => {
+    const dbAccounts = await db.Account.bulkCreate(
+      accounts.map((account, index) => ({
+        name: account.name,
+        desiredAllocationPercentage: percentages[index]
+      }), { transaction: t })
+    )
+  
+    await db.Operation.bulkCreate(
+      accounts.map((account, index) => ({
+        type: 'allocation',
+        amount: account.amount,
+        accountId: dbAccounts[index].id,
+        date: ALLOCATION_DATE
+    })), { transaction: t })
+  })
 
-  await db.Operation.bulkCreate(
-    accounts.map((account, index) => ({
-      type: 'allocation',
-      amount: account.amount,
-      accountId: dbAccounts[index].id,
-      date: ALLOCATION_DATE
-  }))
-  )
 }
 
 module.exports = { 
