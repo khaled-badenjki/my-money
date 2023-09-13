52c9731d9e2b66aa2ec4ca0f0cec6d0c7c73b3fb
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 08:50:57 +0200

    test(green): sip service fails if sip already set

--- src/services/sip.js
+++ src/services/sip.js
@@ -1,6 +1,15 @@
 const db = require('../dal/models')
 
 const execute = async sipAccounts => {
+
+  const account = await db.Account.findOne({
+    where: { monthlyInvestment: { [db.Sequelize.Op.ne]: null } }
+  })
+
+  if (account) {
+    throw new Error('Monthly investment is already set')
+  }
+
   const accounts = Promise.all(
     sipAccounts.map(account => db.Account.update(
       { monthlyInvestment: account.sip },
