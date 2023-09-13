c14bcdef038050fbe8dff13490003b6ae1e6cee5
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 08:45:50 +0200

    test(red): sip service fails if sip already set

--- tests/services/sip.test.js
+++ tests/services/sip.test.js
@@ -1,5 +1,8 @@
 const sinon = require('sinon')
-const { expect } = require('chai')
+const chai = require('chai')
+var chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const { expect } = chai
 const db = require('../../src/dal/models')
 const { sipService } = require('../../src/services')
 
@@ -23,6 +26,7 @@ describe('sip service', () => {
 
   beforeEach(() => {
     accountUpdateStub = sinon.stub(db.Account, 'update')
+      .resolves([1])
   })
 
   afterEach(() => {
@@ -33,5 +37,16 @@ describe('sip service', () => {
     await sipService.execute(accountSip)
 
     expect(accountUpdateStub.called).to.be.true
+  })
+
+  it.only('should fail if monthly investment is already set', async () => {
+    sinon.stub(db.Account, 'findOne').resolves({
+      monthlyInvestment: 1000
+    })
+
+    await expect(sipService.execute(accountSip))
+      .to.be.rejectedWith(Error)
+
+    db.Account.findOne.restore()
   })
 })
