2d4c595d81fe0e95f9518a9e320f576df2414c0b
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-13 07:35:29 +0200

    refactor: add transaction to allocate service function

--- src/services/allocate.js
+++ src/services/allocate.js
@@ -3,29 +3,34 @@ const db = require('../dal/models')
 
 const ALLOCATION_DATE = '2023-01-15'
 
-
 const execute = async accounts => {
   const amounts = accounts.map(account => account.amount)
   const percentages = calculator.calculatePercentages(amounts)
 
+
   await db.sequelize.transaction(async t => {
     const dbAccounts = await db.Account.bulkCreate(
-      accounts.map((account, index) => ({
-        name: account.name,
-        desiredAllocationPercentage: percentages[index]
-      }), { transaction: t })
-    )
+      _buildAccounts(accounts, percentages), { transaction: t })
   
     await db.Operation.bulkCreate(
-      accounts.map((account, index) => ({
-        type: 'allocation',
-        amount: account.amount,
-        accountId: dbAccounts[index].id,
-        date: ALLOCATION_DATE
-    })), { transaction: t })
+      _buildOperations(accounts, dbAccounts), { transaction: t })
   })
-
 }
+
+const _buildAccounts = (accounts, percentages) => 
+  accounts.map((account, index) => ({
+    name: account.name,
+    desiredAllocationPercentage: percentages[index]
+  })
+)
+
+const _buildOperations = (accounts, dbAccounts) => 
+  accounts.map((account, index) => ({
+    type: 'allocation',
+    amount: account.amount,
+    accountId: dbAccounts[index].id,
+    date: ALLOCATION_DATE
+}))
 
 module.exports = { 
   execute
