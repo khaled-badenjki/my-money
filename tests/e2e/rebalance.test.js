26aadb3c44a2d28d55934c3734af054bb86eb9e8
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-14 21:10:08 +0200

    test(red): rebalance calculates rebalance correctly

--- tests/e2e/rebalance.test.js
+++ tests/e2e/rebalance.test.js
@@ -24,6 +24,28 @@ describe('rebalance e2e', () => {
 
     await runCommand('REBALANCE', [])
 
-    expect(logger.info.calledOnceWith('CANNOT_REBALANCE')).to.be.true
+    expect(logger.error.calledOnceWith('CANNOT_REBALANCE')).to.be.true
+  })
+
+  it('should calculate rebalance amount correctly', async () => {
+    await runCommand('ALLOCATE', ['6000', '3000', '1000'])
+
+    await runCommand('SIP', ['2000', '1000', '500'])
+
+    await runCommand('CHANGE', ['4.00%', '10.00%', '2.00%', 'JANUARY'])
+
+    await runCommand('CHANGE', ['--', '-10.00%', '40.00%', '0.00%', 'FEBRUARY'])
+
+    await runCommand('CHANGE', ['12.50%', '12.50%', '12.50%', 'MARCH'])
+
+    await runCommand('CHANGE', ['--', '8.00%', '-3.00%', '7.00%', 'APRIL'])
+
+    await runCommand('CHANGE', ['13.00%', '21.00%', '10.50%', 'MAY'])
+
+    await runCommand('CHANGE', ['--', '10.00%', '8.00%', '-5.00%', 'JUNE'])
+
+    await runCommand('REBALANCE', [])
+
+    expect(logger.info.calledWith('23619 11809 3936')).to.be.true
   })
 })
