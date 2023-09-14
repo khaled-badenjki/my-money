b548f591f278ff52fc3612a403fdf20460e9de31
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-14 21:09:48 +0200

    test(green): rebalance throws error in not enough data

--- src/services/rebalance.js
+++ src/services/rebalance.js
@@ -2,9 +2,12 @@ const db = require('../dal/models')
 
 const execute = async () => {
   const operations = await db.Operation.findAll({
+    where: {
+      type: 'allocation'
+    }
   })
 
-  if (operations.length === 0) {
+  if (operations.length < 6) {
     throw new Error('CANNOT_REBALANCE')
   }
 
