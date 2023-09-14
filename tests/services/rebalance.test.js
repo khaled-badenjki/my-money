709aed3666921ef681b2d4693090ece364613b79
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-14 12:43:45 +0200

    test(red): rebalance service throws an error if no operations were found

---
+++ tests/services/rebalance.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+const { rebalanceService } = require('../../src/services')
+const db = require('../../src/dal/models')
+
+describe('services/rebalance', () => {
+  it('should throw an error "CANNOT_REBALANCE"' + 
+    ' if no records are found in December or June', async () => {
+    
+    sinon.stub(db.Operation, 'findAll').resolves([])
+
+    await expect(rebalanceService.execute()).to.be.rejectedWith(
+      'CANNOT_REBALANCE'
+    )
+
+    db.Operation.findAll.restore()
+  })
+})
