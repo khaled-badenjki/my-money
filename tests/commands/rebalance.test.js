6ac8436ec03962e4c6c527b7547f07356b95a15f
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-14 10:09:49 +0200

    test(red): rebalance command calls the service

---
+++ tests/commands/rebalance.test.js
@@ -0,0 +1,38 @@
+const sinon = require('sinon')
+const { expect } = require('chai')
+const logger = require('../../src/helpers/logger')
+const program = require('../../src/commands')
+const { rebalanceService } = require('../../src/services')
+const { months } = require('../../config')
+
+
+const callRebalance = args => 
+  program.parseAsync(['node', 'index.js', 'REBALANCE'])
+
+describe('commands/rebalance', () => {
+  let loggerStub
+  let rebalanceServiceStub
+  
+  describe('success', () => {
+    beforeEach(() => {
+      loggerStub = sinon.stub(logger, 'info')
+      rebalanceServiceStub = sinon
+        .stub(rebalanceService, 'execute').returns([
+          { name: 'equity', rebalance: 1000 },
+          { name: 'debt', rebalance: 5000 },
+          { name: 'gold', rebalance: 2000 },
+      ])
+    })
+  
+    afterEach(() => {
+      loggerStub.restore()
+      rebalanceServiceStub.restore()
+    })
+    it('should call the rebalanceService.execute', () => {
+      callRebalance()
+      expect(rebalanceServiceStub.calledOnce).to.be.true
+      expect(rebalanceServiceStub.args[0][0]).to.not.exist
+    })
+  })
+
+})
