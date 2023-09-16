d26872d039c30a1a80ecdc5670f72213a7524518
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-16 08:38:04 +0200

    refactor: allocation service

--- src/services/allocate.js
+++ src/services/allocate.js
@@ -1,35 +1,24 @@
 const calculator = require('../helpers/calculator')
 const db = require('../dal/models')
+const {errors, defaults, constants} = require('../../config')
 
-
-const ALLOCATION_DATE = '2023-01-15'
-
-const execute = async accounts => {
-
+const execute = async (accounts) => {
   await validateNonAllocated()
 
   const percentages = calculateDesiredPercentages(accounts)
 
   await persistAsTransaction(accounts, percentages)
-
 }
 
 const validateNonAllocated = async () => {
-  const existingAllocations = await db.Operation.findAll({
-    attributes: ['id'],
-    where: {
-      type: 'allocation'
-    },
-    raw: true
-  })
+  const existingAllocations = await getExistingAllocations()
 
-  if (existingAllocations.length > 0) {
-    throw new Error('ALREADY_ALLOCATED')
-  }
+  if (existingAllocations.length > 0) throw new Error(errors.ALREADY_ALLOCATED)
 }
 
-const calculateDesiredPercentages = accounts => {
-  const amounts = accounts.map(account => account.amount)
+const calculateDesiredPercentages = (accounts) => {
+  const amounts = accounts.map((account) => account.amount)
+
   return calculator.calculatePercentages(amounts)
 }
 
@@ -37,29 +26,35 @@ const persistAsTransaction = async (accounts, percentages) => {
   const t = await db.sequelize.transaction()
 
   const dbAccounts = await db.Account.bulkCreate(
-    _buildAccounts(accounts, percentages), { transaction: t })
+      _buildAccounts(accounts, percentages), {transaction: t})
 
   await db.Operation.bulkCreate(
-    _buildOperations(accounts, dbAccounts), { transaction: t })
+      _buildOperations(accounts, dbAccounts), {transaction: t})
 
   await t.commit()
 }
 
-const _buildAccounts = (accounts, percentages) => 
+const _buildAccounts = (accounts, percentages) =>
   accounts.map((account, index) => ({
     name: account.name,
-    desiredAllocationPercentage: percentages[index]
-  })
-)
+    desiredAllocationPercentage: percentages[index],
+  }),
+  )
 
-const _buildOperations = (accounts, dbAccounts) => 
-  accounts.map((account, index) => ({
-    type: 'allocation',
+const _buildOperations = (newAccounts, existingAccounts) =>
+  newAccounts.map((account, index) => ({
+    type: constants.ALLOCATION,
     amount: account.amount,
-    accountId: dbAccounts[index].id,
-    date: ALLOCATION_DATE
-}))
+    accountId: existingAccounts[index].id,
+    date: defaults.ALLOCATION_DATE,
+  }))
 
-module.exports = { 
-  execute
+const getExistingAllocations = () => db.Operation.findAll({
+  attributes: ['id'],
+  where: {type: constants.ALLOCATION},
+  raw: true,
+})
+
+module.exports = {
+  execute,
 }
