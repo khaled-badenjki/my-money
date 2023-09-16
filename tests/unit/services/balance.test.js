6af6b5687f1240e2690db2e6e95269dfbffa1634
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-09-16 15:42:32 +0200

    test(red): refactor rebalance service

--- tests/unit/services/balance.test.js
+++ tests/unit/services/balance.test.js
@@ -6,77 +6,64 @@ const {months, defaults} = require('../../../config')
 
 describe('balance service', () => {
   const month = months.APRIL
-  let operationFindAllStub
+  let accountFindAllStub
 
   beforeEach(() => {
-    operationFindAllStub = sinon.stub(db.Operation, 'findAll')
+    accountFindAllStub = sinon.stub(db.Account, 'findAll')
         .resolves(
             [
               {
-                accountId: 1,
-                accountName: 'equity',
-                balance: '6000',
+                id: 1,
+                name: 'equity',
+                monthlyInvestment: 2000,
+                desiredAllocationPercentage: 60,
+                balance: 6000,
               },
               {
-                accountId: 2,
-                accountName: 'debt',
-                balance: '3000',
+                id: 2,
+                name: 'debt',
+                monthlyInvestment: 1000,
+                desiredAllocationPercentage: 30,
+                balance: 3000,
               },
               {
-                accountId: 3,
-                accountName: 'gold',
-                balance: '1000',
+                id: 3,
+                name: 'gold',
+                monthlyInvestment: 500,
+                desiredAllocationPercentage: 10,
+                balance: 1000,
               },
             ],
         )
   })
 
   afterEach(() => {
-    operationFindAllStub.restore()
-  })
-
-  it('should call operation model to get sum of all accounts', async () => {
-    await balanceService.execute(month)
-    expect(operationFindAllStub.called).to.be.true
-    expect(operationFindAllStub.args[0][0]).to.deep.equal({
-      attributes: [
-        'accountId',
-        [db.sequelize.fn('sum', db.sequelize.col('amount')), 'balance'],
-        [db.sequelize.literal('account.name'), 'accountName'],
-      ],
-      include: [{
-        model: db.Account,
-        attributes: [],
-        as: 'account',
-      }],
-      group: ['accountId', 'account.name'],
-      raw: true,
-      where: {
-        date: {
-          [db.Sequelize.Op.lte]:
-            `${defaults.YEAR}-${month}-${parseInt(defaults.DAY)+1}`,
-        },
-      },
-    })
+    accountFindAllStub.restore()
   })
 
   it('should return array of account names and their balances', async () => {
     const result = await balanceService.execute(month)
     expect(result).to.deep.equal([
       {
-        accountId: 1,
-        accountName: 'equity',
-        balance: '6000',
+        id: 1,
+        name: 'equity',
+        balance: 6000,
+        monthlyInvestment: 2000,
+        desiredAllocationPercentage: 60,
       },
       {
-        accountId: 2,
-        accountName: 'debt',
-        balance: '3000',
+        id: 2,
+        name: 'debt',
+        balance: 3000,
+        monthlyInvestment: 1000,
+        desiredAllocationPercentage: 30,
       },
       {
-        accountId: 3,
-        accountName: 'gold',
-        balance: '1000',
+        id: 3,
+        name: 'gold',
+        balance: 1000,
+        monthlyInvestment: 500,
+        desiredAllocationPercentage: 10,
       },
     ])
   })
